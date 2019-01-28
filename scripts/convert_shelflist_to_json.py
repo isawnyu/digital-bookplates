ad02b03c66a712bbbae4b006bf6faa87021e8686
Patrick J. Burns <patrick@diyclassics.org>
2019-01-28 12:03:10 -0500

    Update conversion scripts

--- scripts/convert_shelflist_to_json.py
+++ scripts/convert_shelflist_to_json.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 # Imports
 
 import pandas
@@ -7,17 +13,33 @@ from pprint import pprint
 
 import pickle
 
+
+# In[2]:
+
+
 # Pandas expects this file to be encoded as UTF-8 (and possibily to have Unix (LF)-style line breaks. Open csv in Textwrangler vel sim and change this is read_csv generates an error.
 
-df = pandas.read_csv('../data/shelflist.csv')
+df = pandas.read_csv('../app/static/data/shelflist.csv')
+
+
+# In[3]:
+
 
 # Keep only needed columns
 
 df = df[['BSN','Z13_AUTHOR', 'Z13_TITLE', 'Z13_IMPRINT', 'Z30_NOTE_INTERNAL']]
 
+
+# In[4]:
+
+
 # Add column for 'featured' titles
 
 df['featured'] = df.apply(lambda x: 0, axis=1)
+
+
+# In[5]:
+
 
 # Rename columns
 
@@ -29,9 +51,17 @@ df = df.rename(columns={'BSN': 'bsn',
                        }
               )
 
+
+# In[6]:
+
+
 # Drop rows without collection info
 
 df = df.dropna(subset=['collection'])
+
+
+# In[7]:
+
 
 # Helper function to normalize collection data
 
@@ -47,9 +77,17 @@ def clean_collection(string):
 
     return cleaned.strip()
 
+
+# In[8]:
+
+
 # Clean collection column
 
 df['collection'] = df['collection'].apply(lambda x: clean_collection(x))
+
+
+# In[9]:
+
 
 # Get eligible DBP collections
 
@@ -62,8 +100,19 @@ dpb_elig = list(dbp_elig.keys())
 
 df = df[df['collection'].isin(dpb_elig)]
 
+
+# In[10]:
+
+
+# df
+
+
+# In[11]:
+
+
 # Dump json-like object as pickle to data folder
 # e.g.
+
 # [
 #     {
 #         'bsn': '002057948',
@@ -76,4 +125,4 @@ df = df[df['collection'].isin(dpb_elig)]
 # ]
 
 dbp_data = list(df.T.to_dict().values())
-pickle.dump( dbp_data, open('../data/dbp_data.p', 'wb'))
+pickle.dump( dbp_data, open('../app/static/data/dbp_data.p', 'wb'))
