4758c3b6cc63bbc2d471f9861d8903717f315ec0
Patrick J. Burns <patrick@diyclassics.org>
2019-01-28 14:01:51 -0500

    Update scripts

--- scripts/convert_shelflist_to_json.py
+++ scripts/convert_shelflist_to_json.py
@@ -1,98 +1,84 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 # Imports
 
-import pandas
+import pandas as pd
 
 from collections import Counter
 from pprint import pprint
 
 import pickle
 
-
-# In[2]:
-
-
 # Pandas expects this file to be encoded as UTF-8 (and possibily to have Unix (LF)-style line breaks. Open csv in Textwrangler vel sim and change this is read_csv generates an error.
 
-df = pandas.read_csv('../app/static/data/shelflist.csv')
-
-
-# In[3]:
-
+df = pd.read_csv('../app/static/data/shelflist.csv')
 
 # Keep only needed columns
 
 df = df[['BSN','Z13_AUTHOR', 'Z13_TITLE', 'Z13_IMPRINT', 'Z30_NOTE_INTERNAL']]
 
-
-# In[4]:
-
-
-# Add column for 'featured' titles
-
-df['featured'] = df.apply(lambda x: 0, axis=1)
-
-
-# In[5]:
-
-
 # Rename columns
 
-df = df.rename(columns={'BSN': 'bsn',
-                        'Z13_AUTHOR': 'author',
-                        'Z13_TITLE': 'title',
-                        'Z13_IMPRINT': 'imprint',
+df = df.rename(columns={'BSN': 'bsn', 
+                        'Z13_AUTHOR': 'author', 
+                        'Z13_TITLE': 'title', 
+                        'Z13_IMPRINT': 'imprint', 
                         'Z30_NOTE_INTERNAL': 'collection'
                        }
               )
-
-
-# In[6]:
-
 
 # Drop rows without collection info
 
 df = df.dropna(subset=['collection'])
 
-
-# In[7]:
-
-
 # Helper function to normalize collection data
 
 def clean_collection(string):
     cleaned = string
-
+    
     # Remove punctuation with translate
     punctuation ="\"#$%&\'()+,-/:;<=>@[\]^_`{|}~.?!«»—"
     translator = str.maketrans({key: " " for key in punctuation})
     cleaned = cleaned.translate(translator)
-
+    
     cleaned = cleaned.replace('Collection', '')
-
+    
     return cleaned.strip()
-
-
-# In[8]:
-
 
 # Clean collection column
 
 df['collection'] = df['collection'].apply(lambda x: clean_collection(x))
 
+# Helper function to normalize bsns
 
-# In[9]:
+def normalize_bsn(bsn):
+    bsn = str(bsn)
+    if len(bsn) < 9:
+        bsn = "0" * (9-len(bsn)) + bsn
+    return bsn
 
+# Fix bsn length
+
+df['bsn'] = df['bsn'].apply(lambda x: normalize_bsn(x))
+
+# Get featured bsns
+
+with open('../app/static/data/featured.txt') as f:
+    featured_bsns = f.read().splitlines()
+
+# Helper function for add featured bsns
+
+def add_featured(bsn):
+    if bsn in featured_bsns:
+        return 1
+    else:
+        return 0
+
+df['featured'] = df['bsn'].apply(lambda x: add_featured(x))
 
 # Get eligible DBP collections
 
 min_threshold = 100
-excluded_categories = ['Mesopotamian', 'Bobst transfer']
+excluded_categories = ['Mesopotamian', 'Bobst transfer', "LP", "Persian"]
 
 dbp_elig = Counter(df['collection'])
 dbp_elig = {x : dbp_elig[x] for x in dbp_elig if dbp_elig[x] >= min_threshold and x not in excluded_categories}
@@ -100,15 +86,13 @@ dpb_elig = list(dbp_elig.keys())
 
 df = df[df['collection'].isin(dpb_elig)]
 
+# Remove nan
 
-# In[10]:
+df = df.where((pd.notnull(df)), None)
 
+# Drop dupes
 
-# df
-
-
-# In[11]:
-
+df.drop_duplicates(keep='first', inplace=True)
 
 # Dump json-like object as pickle to data folder
 # e.g.
@@ -126,3 +110,5 @@ df = df[df['collection'].isin(dpb_elig)]
 
 dbp_data = list(df.T.to_dict().values())
 pickle.dump( dbp_data, open('../app/static/data/dbp_data.p', 'wb'))
+
+print("Conversion complete!")
