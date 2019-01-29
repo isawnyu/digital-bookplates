4722a62ccc761ae1d68c9f6e5c71c15b79fdbb6f
Patrick J. Burns <patrick@diyclassics.org>
2019-01-29 11:22:26 -0500

    Update config files

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
 export FLASK_APP=bookplates.py
-export FLASK_DEBUG=1
+export APP_SETTINGS="config.DevelopmentConfig"
--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
+import os
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 bootstrap = Bootstrap(app)
 
 from app import routes
