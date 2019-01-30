84c3307b9980d86d285d51ed941ee8bac15d8d44
Patrick J. Burns <patrick@diyclassics.org>
2019-01-30 13:13:53 -0500

    Add static folder

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app._static_folder = 'static'
 app.config.from_object(os.environ['APP_SETTINGS'])
 bootstrap = Bootstrap(app)
 
